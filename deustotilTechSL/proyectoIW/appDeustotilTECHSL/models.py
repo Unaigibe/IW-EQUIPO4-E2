c2216a6c4782f81387776883f8e33c1c434628d3
unaigibe <unaigibe@opendeusto.es>
2020-04-08 11:02:46 +0200

    Definicion de Modelos

--- deustotilTechSL/proyectoIW/appDeustotilTECHSL/models.py
+++ deustotilTechSL/proyectoIW/appDeustotilTECHSL/models.py
@@ -1,18 +1,31 @@
 from django.db import models
 
-class Empleado(models.Model):
+class Prioridad(models.Model):
+    prioridad = models.CharField(max_length=15)
 
-    dni = models.CharField(max_length=12)
+
+class EstadoTarea(models.Model):
+    estado_tarea = models.CharField(max_length=12)
+
+
+class Empleado(models.Model):
+    dni = models.CharField(max_length=12, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=25)
     apellido1 = nombre = models.CharField(max_length=25)
     apellido2 = models.CharField(max_length=25)
     email = models.EmailField()
     telefono = models.IntegerField(max_length=14)
 
-    class Meta:
-        ordering = ["nombre"]
 
-
+class Tarea(models.Model):
+    nombre = models.CharField(max_length=25, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=120)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    responsable = models.ForeignKey(Empleado, on_delete=models.SET_NULL)
+    prioridad = models.OneToOneField(Prioridad, on_delete=models.SET_NULL)
+    estadoTarea = models.OneToOneField(EstadoTarea, on_delete=models.SET_NULL)
+    nota_adicional = models.TextField(max_length=750)
 
 
 class Cliente(models.Model):
@@ -23,14 +36,14 @@ class Cliente(models.Model):
     telf_cliente = models.IntegerField(max_length=14)
     email_cliente = models.EmailField()
 
-    class Meta:
-        ordering = ["nombre_empresa"]
 
+class Proyecto(models.Model):
+    nombre = models.CharField(max_length=25, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=120)
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField()
+    presupuesto = models.FloatField()
+    cliente = models.OneToOneField(Cliente, on_delete=models.SET_NULL)
+    tareas_a_realizar = models.ManyToManyField(Tarea)
+    empleados = models.ManyToManyField(Empleado)
 
-
-
-class Prioridad(models.Model):
-    prioridad = models.CharField(max_length=15)
-
-class Estado_Tarea(models.Model):
-    estado_tarea = models.ManyToManyField() #PTE DE MIRAR!!!!
