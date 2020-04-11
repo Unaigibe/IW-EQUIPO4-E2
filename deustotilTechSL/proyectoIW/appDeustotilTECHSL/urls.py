40afde4a3ec88660d7392b67ecbfb6eb86ba8372
AsierSojo <sojo.asier@opendeusto.es>
2020-04-11 09:34:10 +0200

    Prueba Añadidas tareas

--- deustotilTechSL/proyectoIW/appDeustotilTECHSL/urls.py
+++ deustotilTechSL/proyectoIW/appDeustotilTECHSL/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     #path('lista_proyectos/crear/', views.ProyectoNewView.as_view(), name='nuevo_proyecto'),
     #path('lista_proyectos/borrar', views.ProyectoDeleteView.as_view(), name='borrar_proyecto'),
     #path('lista_proyectos/editar/', views.ProyectoEditView.as_view(), name='editar_proyecto'),
+
+
     # path('lista_tareas/', views.TareaListView.as_view(), name='lista_tareas'),
     # path('lista_tareas/<int:pk>/', views.TareaDetailView.as_view(), name='tarea'),
     # path('lista_tareas/crear/', views.TareaNewView.as_view(), name='nueva_tarea'),
