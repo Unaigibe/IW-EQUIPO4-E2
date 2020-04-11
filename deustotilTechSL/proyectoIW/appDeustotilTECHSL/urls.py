64f6c257fcbd5a5e63fb56d2d1c3b5f198def579
AsierSojo <sojo.asier@opendeusto.es>
2020-04-11 09:53:31 +0200

    Prueba git

--- deustotilTechSL/proyectoIW/appDeustotilTECHSL/urls.py
+++ deustotilTechSL/proyectoIW/appDeustotilTECHSL/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('home/', views.ProyectoListView.as_view(), name='home'),
     path('lista_proyectos/', views.ProyectoListView.as_view(), name='lista_proyectos'),
-    #path('lista_proyectos/<int:pk>/', views.ProyectoDetailView.as_view(), name='proyecto'),
+
     #path('lista_proyectos/crear/', views.ProyectoNewView.as_view(), name='nuevo_proyecto'),
     #path('lista_proyectos/borrar', views.ProyectoDeleteView.as_view(), name='borrar_proyecto'),
     #path('lista_proyectos/editar/', views.ProyectoEditView.as_view(), name='editar_proyecto'),
@@ -14,13 +14,6 @@ urlpatterns = [
     #     # path('lista_tareas/<int:pk>/', views.TareaDetailView.as_view(), name='tarea'),
     #     # path('lista_tareas/crear/', views.TareaNewView.as_view(), name='nueva_tarea'),
 
-        # path('lista_tareas/', views.TareaListView.as_view(), name='lista_tareas'),
-    #     # path('lista_tareas/<int:pk>/', views.TareaDetailView.as_view(), name='tarea'),
-    #     # path('lista_tareas/crear/', views.TareaNewView.as_view(), name='nueva_tarea'),    # path('lista_tareas/', views.TareaListView.as_view(), name='lista_tareas'),
-    #     #     # path('lista_tareas/<int:pk>/', views.TareaDetailView.as_view(), name='tarea'),
-    #     #     # path('lista_tareas/crear/', views.TareaNewView.as_view(), name='nueva_tarea'),    # path('lista_tareas/', views.TareaListView.as_view(), name='lista_tareas'),
-    #     #     # path('lista_tareas/<int:pk>/', views.TareaDetailView.as_view(), name='tarea'),
-    #     #     # path('lista_tareas/crear/', views.TareaNewView.as_view(), name='nueva_tarea'),    # path('lista_tareas/', views.TareaListView.as_view(), name='lista_tareas'),
-    #     #     # path('lista_tareas/<int:pk>/', views.TareaDetailView.as_view(), name='tarea'),
-    #     #     # path('lista_tareas/crear/', views.TareaNewView.as_view(), name='nueva_tarea'),
+    #path('lista_clientes/<int:pk>/', views.ClienteDetailView.as_view(), name='cliente'),
+
 ]
