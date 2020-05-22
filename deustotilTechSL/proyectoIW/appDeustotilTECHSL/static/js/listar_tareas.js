7120384e4ee5710cea86c16305d9e0a5e76e214d
unaigibe <unaigibe@opendeusto.es>
2020-05-22 14:27:50 +0200

    Entrega Final

--- deustotilTechSL/proyectoIW/appDeustotilTECHSL/static/js/listar_tareas.js
+++ deustotilTechSL/proyectoIW/appDeustotilTECHSL/static/js/listar_tareas.js
@@ -1,4 +1,4 @@
-function loadData() {
+function loadDataTareas() {
     let url = 'http://127.0.0.1:8000/index/api_tareas/'
     fetch(url)
         .then((response) => response.json())
@@ -9,11 +9,11 @@ function loadData() {
 
 function formato(fecha){
   return fecha.replace(/^(\d{4})-(\d{2})-(\d{2})$/g,'$3/$2/$1');
-}
+  }
 
 function limpiarTabla(){
 document.getElementById("tareas").innerHTML=""
-}
+  }
 
 
 function crearTarea(nombre, fecha_inicio, fecha_fin, prioridad, id){
@@ -23,10 +23,6 @@ function crearTarea(nombre, fecha_inicio, fecha_fin, prioridad, id){
     var fecha_inicio_Formateada = formato(fecha_inicio)
     var fecha_fin_Formateada = formato(fecha_fin)
     var prioridadTareaSeleccionada = document.getElementById('prioridad_Tareas').value
-
-    console.log(prioridadTareaSeleccionada)
-    console.log(prioridad)
-    console.log(prioridadTareaSeleccionada == prioridad)
 
     if (prioridadTareaSeleccionada == "Mostrar_todas")
         return `
@@ -46,7 +42,9 @@ function crearTarea(nombre, fecha_inicio, fecha_fin, prioridad, id){
             <td>${fecha_fin_Formateada}</td>
             <td>${prioridad}</td>
             <td><a href="/index/tarea/${id}"><img id="iconoDetalles" src='/static/img/iconoDetalles.png'></a></td>
-        </tr>`;}
+        </tr>`;
+
+        return ""}
 
 function generarHTMLTablaTareas(tareas) {
     let tabla = `
@@ -63,6 +61,8 @@ function generarHTMLTablaTareas(tareas) {
     `;
     for(const tarea of tareas) {
         tabla += crearTarea(tarea.nombre, tarea.fecha_inicio, tarea.fecha_fin, tarea.prioridad, tarea.id);
+        let htmlTarea = crearTarea(tarea.nombre, tarea.fecha_inicio, tarea.fecha_fin, tarea.prioridad, tarea.id);
+        console.log("HTML para la tarea con ID "+tarea.id+": "+htmlTarea);
     }
     tabla += '</tbody></table>'
     return tabla;
@@ -73,7 +73,7 @@ function crearTablaTareas(tareas){
     document.getElementById('tareas').innerHTML = tabla;
 }
 
-loadData()
+loadDataTareas()
 
 function mostrarForm() {
        document.getElementById('formulario').style.display=''
